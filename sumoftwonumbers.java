1ec2af1e30701cf6a906f0cd7722860c36d9f1c1
suji-k-05 <ksujipriya2000@gmail.com>
2025-11-18 16:23:37 +0530

    Update sumoftwonumbers.java

--- sumoftwonumbers.java
+++ sumoftwonumbers.java
@@ -3,20 +3,12 @@ import java.util.Scanner;
 public class sumoftwonumbers {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        int a = 10;
+int b = 20;
+System.out.println(a + b);
 
-        System.out.print("Enter first number: ");
-        int a = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int b = sc.nextInt();
-
-        int sum = a + b;
-
-        System.out.println("Sum = " + sum);
-
-        sc.close();
     }
 }
+
 
 
